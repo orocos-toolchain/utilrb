f365bc9c9e9ab4db0b90ac4003e4b6d4c99c6487
tailor <sylvain.joyeux@laas.fr>
2006-08-02 10:00:10 +0000

    define Logger::Hierarchy and Logger::Forward

---
+++ lib/utilrb/logger/forward.rb
@@ -0,0 +1,15 @@
+class Logger
+    # Forward logger output methods to the logger attribute, so that
+    # we can do
+    #	MyModule.debug "debug_info"
+    # instead of 
+    #   MyModule.logger.debug "debug_info"
+    module Forward
+        [ :debug, :info, :warn, :error, :fatal, :unknown ].each do |level|
+            class_eval <<-EOF
+                def #{level}(*args, &proc); logger.#{level}(*args, &proc) end
+            EOF
+        end
+    end
+end
+
---
+++ lib/utilrb/logger/hierarchy.rb
@@ -0,0 +1,32 @@
+class Logger
+    # Define a hierarchy of loggers mapped to the module hierarchy.
+    # It defines the #logger accessor which either returns the logger
+    # attribute of the module, if one is defined, or its parent logger
+    # attribute.
+    # 
+    # module First
+    #   include Hierarchy
+    #   self.logger = Logger.new
+    #
+    #   module Second
+    #     include Hierarchy
+    #   end
+    # end
+    #
+    # Second.logger will return First.logger. If we do
+    # Second.logger = Logger.new, then this one would
+    # be returned.
+    module Hierarchy
+        attr_writer :logger
+        def logger
+            return @logger if defined?(@logger) && @logger
+            if kind_of?(Module)
+                constant(self.dirname).logger
+            else
+                self.class.logger
+            end
+        end
+    end
+end
+
+
