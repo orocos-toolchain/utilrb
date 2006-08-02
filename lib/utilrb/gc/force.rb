f37f01d777c272ea05b1f38d167b0cd89432fa42
tailor <sylvain.joyeux@laas.fr>
2006-08-02 11:24:13 +0000

    fix GC#force since GC.enable returns false when the GC is already enabled while GC.disable returns true if it was already disabled

--- lib/utilrb/gc/force.rb
+++ lib/utilrb/gc/force.rb
@@ -1,10 +1,10 @@
 module GC
     # Forcefully starts the GC even when GC.disable has been called
     def self.force
-	enabled_gc = GC.enable
+	disabled_gc = GC.enable
 	GC.start
     ensure
-	GC.disable unless enabled_gc
+	GC.disable if disabled_gc
     end
 end
 
---
+++ test/test_gc.rb
@@ -0,0 +1,40 @@
+require 'test/unit'
+require 'test_config'
+
+require 'utilrb/gc'
+require 'enumerator'
+
+class TC_GC < Test::Unit::TestCase
+    def allocate(&block)
+	ObjectSpace.define_finalizer(Object.new, &block)
+	nil
+    end
+
+    def allocate(&block)
+	# Allocate twice since it seems the last object stays on stack
+	# (and is not GC'ed)
+	2.times { ObjectSpace.define_finalizer(Object.new, &block) }
+	nil
+    end
+
+    def test_force
+	finalized = false
+	allocate { finalized = true }
+	GC.start
+	assert( finalized )
+
+	GC.disable
+	finalized = false
+	allocate { finalized = true }
+	GC.start
+	assert( !finalized )
+	GC.force
+	assert( finalized )
+	assert( GC.disable )
+
+	GC.enable
+	GC.force
+	assert( !GC.enable )
+    end
+end
+
