47a4352f4b99879501d6f073680ea91a46a69dd0
tailor <sylvain.joyeux@laas.fr>
2006-09-04 09:39:27 +0000

    make UTILRB_FASTER_MODE a constant instead of a global variable

--- lib/utilrb/common.rb
+++ lib/utilrb/common.rb
@@ -1,18 +1,18 @@
-unless defined? $UTILRB_FASTER_MODE
+unless defined? UTILRB_FASTER_MODE
     if ENV['UTILRB_FASTER_MODE'] == 'no'
-	$UTILRB_FASTER_MODE = nil
+	UTILRB_FASTER_MODE = nil
 	STDERR.puts "Utilrb: not loading the C extension"
     else
 	begin
 	    require 'utilrb/faster'
-	    $UTILRB_FASTER_MODE = true
+	    UTILRB_FASTER_MODE = true
 	    STDERR.puts "Utilrb: loaded C extension" if ENV['UTILRB_FASTER_MODE']
 	rescue LoadError => e
 	    raise unless e.message =~ /no such file to load/
 	    if ENV['UTILRB_FASTER_MODE'] == 'yes'
 		raise LoadError, "unable to load Util.rb C extension: #{e.message}"
 	    else
-		$UTILRB_FASTER_MODE = nil
+		UTILRB_FASTER_MODE = nil
 	    end
 	end
     end
@@ -20,10 +20,10 @@ end
 
 module Kernel
     def unless_faster
-	return yield unless $UTILRB_FASTER_MODE
+	return yield unless UTILRB_FASTER_MODE
     end
     def require_faster(name)
-	if $UTILRB_FASTER_MODE
+	if UTILRB_FASTER_MODE
 	    yield
 	else
 	    STDERR.puts "Utilrb: not loading #{name} since the C extension is not available"
