607aae854dda1c59fe35db9e2a8fedcad8072c46
tailor <sylvain.joyeux@laas.fr>
2006-11-30 17:28:16 +0000

    [queue] make Queue#get return [] if the queue is empty and non_block is true

--- lib/utilrb/queue/get.rb
+++ lib/utilrb/queue/get.rb
@@ -1,7 +1,10 @@
 class Queue
+    # Returns all elements currently in the queue.
+    # If +non_block+ is true, returns an empty array
+    # if the queue is empty
     def get(non_block = false)
 	while (Thread.critical = true; @que.empty?)
-	    raise ThreadError, "queue empty" if non_block
+	    return [] if non_block
 	    @waiting.push Thread.current
 	    Thread.stop
 	end
