88a70fd742fb30a1c11284ba9766fc8af142cfa6
tailor <sylvain.joyeux@laas.fr>
2007-01-13 10:04:39 +0000

    [queue] do not define Queue#get if it is already defined (needed to use fastthread)

--- lib/utilrb/queue/get.rb
+++ lib/utilrb/queue/get.rb
@@ -1,17 +1,18 @@
 class Queue
-    # Returns all elements currently in the queue.
-    # If +non_block+ is true, returns an empty array
-    # if the queue is empty
-    def get(non_block = false)
-	while (Thread.critical = true; @que.empty?)
-	    return [] if non_block
-	    @waiting.push Thread.current
-	    Thread.stop
+    # Returns all elements currently in the queue.  If +non_block+ is true,
+    # returns an empty array if the queue is empty
+    unless instance_methods.include?("get")
+	def get(non_block = false)
+	    while (Thread.critical = true; @que.empty?)
+		return [] if non_block
+		@waiting.push Thread.current
+		Thread.stop
+	    end
+	    result = @que.dup
+	    @que.clear
+	    result
+	ensure
+	    Thread.critical = false
 	end
-	result = @que.dup
-	@que.clear
-	result
-    ensure
-	Thread.critical = false
     end
 end
