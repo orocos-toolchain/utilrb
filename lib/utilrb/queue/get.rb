100c95f0ba101eb3ed02b5de1e1824690a205769
tailor <sylvain.joyeux@laas.fr>
2007-01-25 11:14:27 +0000

    [queue] include the patch needed to define Queue#get on fastthread

--- lib/utilrb/queue/get.rb
+++ lib/utilrb/queue/get.rb
@@ -1,6 +1,10 @@
 class Queue
     # Returns all elements currently in the queue.  If +non_block+ is true,
     # returns an empty array if the queue is empty
+    #
+    # WARNING: this method is NOT compatible with fastthread's Queue implementation
+    # If you rely on Queue#get and want to use fastthread, you will have to patch
+    # fastthread with patches/fastthread-queue-get.patch
     unless instance_methods.include?("get")
 	def get(non_block = false)
 	    while (Thread.critical = true; @que.empty?)
