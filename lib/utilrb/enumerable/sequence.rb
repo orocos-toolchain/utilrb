fafafbbde7227678062ba8d2ed3a96998bb7d240
tailor <sylvain.joyeux@m4x.org>
2007-02-12 20:33:39 +0000

    [enumerable] define #+ on enumerator only

--- lib/utilrb/enumerable/sequence.rb
+++ lib/utilrb/enumerable/sequence.rb
@@ -1,8 +1,16 @@
-# An enumerator which iterates on a sequence of enumerator
+require 'enumerator'
+
+# An enumerator which iterates on multiple iterators in sequence
+#
+#   ([1, 2].enum_for + [2, 3].enum_for).each => 1, 2, 2, 3
+#
+# See also Enumerator#+
 class SequenceEnumerator
     def initialize; @sequence = Array.new end
+
+    def +(other); SequenceEnumerator.new << self << other end
     # Adds +object+ at the back of the sequence
-    def <<(object); @sequence << object; self end
+    def <<(other); @sequence << other; self end
 
     def each
 	@sequence.each { |enum| enum.each { |o| yield(o) } } if block_given?
@@ -12,7 +20,7 @@ class SequenceEnumerator
     include Enumerable
 end
 
-module Enumerable # :nodoc
+class Enumerable::Enumerator # :nodoc
     # Builds a sequence of enumeration object.
     #	([1, 2].enum_for + [2, 3]).each		=> 1, 2, 2, 3
     def +(other_enumerator) # :nodoc
