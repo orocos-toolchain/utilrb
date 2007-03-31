e4fc0503f4cdc5d3942750ebac75b1621996ee9c
tailor <sylvain.joyeux@m4x.org>
2007-03-31 09:46:09 +0000

    [exception] add the 'since' and 'until' options to Exception#full_message

--- lib/utilrb/exception/full_message.rb
+++ lib/utilrb/exception/full_message.rb
@@ -1,3 +1,4 @@
+require 'utilrb/kernel/options'
 
 class Exception
     # Returns the full exception message, with backtrace, like the one we get from
@@ -16,12 +17,40 @@ class Exception
     #   test.rb:3:in `test': this is a test (RuntimeError)
     #         from test.rb:7
     #
-    def full_message(&block)
-	first, *remaining = if block_given? then backtrace.find_all(&block)
-			    else backtrace
+    # Two regular expressions can be given through the +options+ hash to filter the backtrace:
+    # since:: 
+    #   Only the methods *below* the first matching line will be displayed. The matching
+    #   line is included.
+    # until::
+    #   Only the methods *above* the first matching line will be displayed. The matching
+    #   line is *not* included
+    #
+    # If a block is given, each line of the backtrace are yield and only the lines for which
+    # the block returns true are displayed
+    #
+    def full_message(options = {}, &block)
+	options = validate_options options, [:since, :until]
+	since_matches, until_matches = options[:since], options[:until]
+
+	trace = backtrace
+	if since_matches || until_matches
+	    found_beginning, found_end = !since_matches, false
+	    trace = trace.find_all do |line|
+		found_beginning ||= (line =~ since_matches)
+		found_end       ||= (line =~ until_matches) if until_matches
+		found_beginning && !found_end
+	    end
+	end
+
+	first, *remaining = if block_given? then trace.find_all(&block)
+			    else trace
 			    end
 
-	"#{first}: #{message} (#{self.class})\n\tfrom " + remaining.join("\n\tfrom ")
+	msg = "#{first}: #{message} (#{self.class})"
+	unless remaining.empty?
+	    msg << "\n\tfrom " + remaining.join("\n\tfrom ")
+	end
+	msg
     end
 end
 
--- test/test_exception.rb
+++ test/test_exception.rb
@@ -5,14 +5,33 @@ require 'flexmock'
 class TC_Exception < Test::Unit::TestCase
     def test_full_message
 	FlexMock.use do |mock|
-	    msg = begin
-		      raise "this is an error"
-		  rescue RuntimeError => error
-		      error.full_message { |trace| trace !~ /test\/unit\// }
-		  end
+	    error = Exception.new
+	    def error.message; "this is an error" end
+	    def error.backtrace
+		(0..10).map { |i| i.to_s }
+	    end
 
-	    lines = msg.split("\n")
-	    assert_equal(4, lines.size, lines)
+	    full     = error.full_message.split("\n")
+	    assert_equal(11, full.size)
+
+	    filtered = error.full_message { |line| Integer(line) % 2 == 0 }.split("\n")
+	    assert_equal(6, filtered.size)
+	    assert_equal(full.values_at(0, 2, 4, 6, 8, 10), filtered)
+
+	    since_matches = error.full_message(:since => /4/).split("\n")
+	    assert_equal(7, since_matches.size)
+	    assert_equal(full.values_at(*(5..10)), since_matches[1..-1])
+
+	    until_matches = error.full_message(:until => /3/).split("\n")
+	    assert_equal(3, until_matches.size)
+	    assert_equal(full.values_at(1, 2), until_matches[1..-1])
+
+	    since_and_until = error.full_message(:since => /3/, :until => /6/).split("\n")
+	    assert_equal(3, since_and_until.size)
+	    assert_equal(full.values_at(4, 5), since_and_until[1..-1])
+
+	    all = error.full_message(:since => /3/, :until => /6/) { |line| Integer(line) % 2 == 0 }.split("\n")
+	    assert_equal(1, all.size)
 	end
     end
 end
