38411024537eca1342389ea4900cb15bbbee3e4f
tailor <sylvain.joyeux@m4x.org>
2007-03-31 10:19:09 +0000

    [module,doc] fix documentation of Module#has_ancestor?: it works for singleton classes in all cases now

--- lib/utilrb/module/ancestor_p.rb
+++ lib/utilrb/module/ancestor_p.rb
@@ -2,17 +2,13 @@ require 'utilrb/common'
 class Module
     # Check if +klass+ is an ancestor of this class/module
     #
-    # If Utilrb's C extension is built, works for singleton classes as well:
+    # It works for singleton classes as well:
     #
     #   class MyClass; end
     #   obj       = MyClass.new
     #   singleton = class << obj; obj end
     #
-    # With the C extension:
     #   singleton.has_ancestor?(MyClass) # => true 
-    #
-    # without it
-    #   singleton.has_ancestor?(MyClass) # => false 
     #
     Utilrb.if_faster do
 	def has_ancestor?(klass)
