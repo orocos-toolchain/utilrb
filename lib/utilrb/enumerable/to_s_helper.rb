2e256f008d002909e7fbd1a6eb67ef7f65fb5173
tailor <sylvain.joyeux@m4x.org>
2007-04-01 13:32:25 +0000

    [enumerable] fix the generic to_s implementation

--- lib/utilrb/enumerable/to_s_helper.rb
+++ lib/utilrb/enumerable/to_s_helper.rb
@@ -2,11 +2,11 @@ module EnumerableToString
     # This method is a generic implementaion of #to_s on enumerables.
     def self.to_s_helper(enumerable, start, stop, &block)
 	stack = (Thread.current[:to_s_helper] ||= [])
-	if stack.include?(object_id)
+	if stack.include?(enumerable.object_id)
 	    "..."
 	else
 	    begin
-		stack.push object_id
+		stack.push enumerable.object_id
 		start.dup << enumerable.map(&block).join(", ") << stop
 	    ensure
 		stack.pop
