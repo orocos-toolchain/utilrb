7d3db214f9d1b5ea5252a24cd9aa5d1969653ee9
tailor <sylvain.joyeux@laas.fr>
2007-05-08 11:56:31 +0000

    [enumerable] in .to_s_helper, do not create a Proc object: use yield instead of &iterator

--- lib/utilrb/enumerable/to_s_helper.rb
+++ lib/utilrb/enumerable/to_s_helper.rb
@@ -1,13 +1,13 @@
 module EnumerableToString
     # This method is a generic implementaion of #to_s on enumerables.
-    def self.to_s_helper(enumerable, start, stop, &block)
+    def self.to_s_helper(enumerable, start, stop)
 	stack = (Thread.current[:to_s_helper] ||= [])
 	if stack.include?(enumerable.object_id)
 	    "..."
 	else
 	    begin
 		stack.push enumerable.object_id
-		start.dup << enumerable.map(&block).join(", ") << stop
+		start.dup << enumerable.map { |el| yield(el) }.join(", ") << stop
 	    ensure
 		stack.pop
 	    end
