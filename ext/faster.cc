f3f1892a3be8153b63eb95a55e647d55d16e528c
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2008-03-13 12:09:00 +0100

    [1.9] disable Proc extensions and IO#clearerr
      I disable them for now since I don't have time to check
      how they are to be implemented in 1.9

--- ext/faster.cc
+++ ext/faster.cc
@@ -32,19 +32,6 @@ static VALUE enumerable_each_uniq(VALUE self)
 }
 
 /* call-seq:
- *  io.clearerr => nil 
- *
- * Clear all error flags on the IO
- */
-static VALUE io_clearerr(VALUE self)
-{
-    OpenFile* file;
-    file = RFILE(self)->fptr;
-    clearerr(file->f);
-    return Qnil;
-}
-
-/* call-seq:
  *  Kernel.is_singleton?(object)
  *
  * Returns true if +self+ is a singleton class 
@@ -57,59 +44,6 @@ static VALUE kernel_is_singleton_p(VALUE self)
 	return Qfalse;
 }
 
-
-/* call-seq:
- *  proc.same_body?(other) => true or false
- *
- * Returns true if +self+ and +other+ have the same body
- */
-static VALUE proc_same_body_p(VALUE self, VALUE other)
-{
-    if (self == other) return Qtrue;
-    if (TYPE(other) != T_DATA) return Qfalse;
-    if (RDATA(other)->dmark != RDATA(self)->dmark) return Qfalse;
-    if (CLASS_OF(self) != CLASS_OF(other)) return Qfalse;
-
-    struct BLOCK* data, *data2;
-    Data_Get_Struct(self, struct BLOCK, data);
-    Data_Get_Struct(other, struct BLOCK, data2);
-    return (data->body == data2->body) ? Qtrue : Qfalse;
-}
-
-/* call-seq:
- *  proc.file
- *
- * Returns the file in which the proc body is defined, or nil
- */
-static VALUE proc_file(VALUE self)
-{ 
-    struct BLOCK *data;
-    NODE *node;
-
-    Data_Get_Struct(self, struct BLOCK, data);
-    if ((node = data->frame.node) || (node = data->body)) 
-	return rb_str_new2(node->nd_file);
-    else 
-	return Qnil;
-}
-
-/* call-seq:
- *  proc.line
- *
- * Returns the line at which the proc body is defined, or nil
- */
-static VALUE proc_line(VALUE self)
-{
-    struct BLOCK *data;
-    NODE *node;
-
-    Data_Get_Struct(self, struct BLOCK, data);
-    if ((node = data->frame.node) || (node = data->body)) 
-	return INT2FIX(nd_line(node));
-    else
-	return Qnil;
-}
-
 extern "C" void Init_value_set();
 extern "C" void Init_swap();
 
@@ -117,10 +51,6 @@ extern "C" void Init_faster()
 {
     rb_define_method(rb_mEnumerable, "each_uniq", RUBY_METHOD_FUNC(enumerable_each_uniq), 0);
     rb_define_method(rb_mKernel, "is_singleton?", RUBY_METHOD_FUNC(kernel_is_singleton_p), 0);
-    rb_define_method(rb_cProc, "same_body?", RUBY_METHOD_FUNC(proc_same_body_p), 1);
-    rb_define_method(rb_cProc, "file", RUBY_METHOD_FUNC(proc_file), 0);
-    rb_define_method(rb_cProc, "line", RUBY_METHOD_FUNC(proc_line), 0);
-    rb_define_method(rb_cIO, "clearerr", RUBY_METHOD_FUNC(io_clearerr), 0);
 
     Init_value_set();
     Init_swap();
