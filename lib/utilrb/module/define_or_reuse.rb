d486eed9f535f4c4bd2e6b164c1727c40b8ab753
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2008-04-11 10:50:22 +0200

    [module] make #define_or_reuse usable on 1.8 and 1.9

--- lib/utilrb/module/define_or_reuse.rb
+++ lib/utilrb/module/define_or_reuse.rb
@@ -1,18 +1,28 @@
 class Module
-    # :call-seq
-    #   define_or_reuse(name, value)   ->              value
-    #   define_or_reuse(name) { ... }  ->              value
-    #
-    # Defines a new constant under a given module, or reuse the
-    # already-existing value if the constant is already defined.
-    #
-    # In the first form, the method gets its value from its argument. 
-    # In the second case, it calls the provided block
-    def define_or_reuse(name, value = nil)
-        if const_defined?(name, false)
-            const_get(name)
-        else
-            const_set(name, (value || yield))
+    if Utilrb::RUBY_IS_19
+        # :call-seq
+        #   define_or_reuse(name, value)   ->              value
+        #   define_or_reuse(name) { ... }  ->              value
+        #
+        # Defines a new constant under a given module, or reuse the
+        # already-existing value if the constant is already defined.
+        #
+        # In the first form, the method gets its value from its argument. 
+        # In the second case, it calls the provided block
+        def define_or_reuse(name, value = nil)
+            if const_defined?(name, false)
+                const_get(name)
+            else
+                const_set(name, (value || yield))
+            end
+        end
+    else
+        def define_or_reuse(name, value = nil)
+            if const_defined?(name)
+                const_get(name)
+            else
+                const_set(name, (value || yield))
+            end
         end
     end
 end
