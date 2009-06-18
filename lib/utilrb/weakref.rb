0d40fb7184bc873f96b576bd4acbcdcaa0ca17d8
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2009-06-18 14:59:33 +0200

    weakref: add the object finalizer only once
    Right now, each WeakRef#initialize where defining the weakref's
    object finalizer, leading to multiple calls to it in case multiple
    weakrefs have been defined on it.
    
    It was harmless, just ugly.

--- lib/utilrb/weakref.rb
+++ lib/utilrb/weakref.rb
@@ -7,7 +7,9 @@ Utilrb.require_ext("Utilrb::WeakRef") do
                 if obj.kind_of?(WeakRef)
                     raise ArgumentError, "cannot create a weakref of a weakref"
                 end
+                unless WeakRef.refcount(obj)
                     ObjectSpace.define_finalizer(obj, self.class.method(:do_object_finalize))
+                end
                 do_initialize(obj)
             end
         end
