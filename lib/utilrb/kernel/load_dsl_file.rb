d5158f32483cabff3d94fcc66bf481f17a0daec4
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2009-12-17 16:46:27 +0100

    module: implement #load_dsl_file_eval
    This is meant to replace #load_dsl_file eventually. It handles
    dynamic constant detection (i.e. proper #const_missing) and is
    more robust with backtrace filtering.

--- lib/utilrb/kernel/load_dsl_file.rb
+++ lib/utilrb/kernel/load_dsl_file.rb
@@ -5,6 +5,99 @@ module Kernel
     #
     # The caller of this method should call it at the end of its definition
     # file, or the translation method may not be robust at all
+    def load_dsl_file_eval(file, proxied_object, context, full_backtrace, *exceptions, &block)
+        loaded_file = file.gsub(/^#{Regexp.quote(Dir.pwd)}\//, '')
+        our_frame_pos = caller.size
+
+        if !File.readable?(file)
+            raise ArgumentError, "#{file} does not exist"
+        end
+        sandbox = Class.new do
+            class << self
+                attr_reader :main_object
+                def method_missing(*args, &block)
+                    main_object.send(*args, &block)
+                end
+            end
+        end
+
+        sandbox.instance_variable_set :@main_object, proxied_object
+        context.each do |mod|
+            sandbox.include mod
+        end
+        if block_given?
+            sandbox.singleton_class.class_eval do
+                define_method(:const_missing, &block)
+            end
+        end
+                
+        sandbox.class_eval(File.read(file))
+
+    rescue Exception => e
+        if exceptions.any? { |e_class| e.kind_of?(e_class) }
+            raise e
+        end
+
+        raise e if full_backtrace
+
+        backtrace = e.backtrace.dup
+        message   = e.message.dup
+
+        # Filter out the message ... it can contain backtrace information as
+        # well (!)
+        message = message.split("\n").map do |line|
+            if line =~ /^.*:\d+(:.*)$/
+                backtrace.unshift line
+                nil
+            else
+                line
+            end
+        end.compact.join("\n")
+
+        if message.empty?
+            message = backtrace.shift
+            if message =~ /^(\s*[^\s]+:\d+:)\s*(.*)/
+                location = $1
+                message  = $2
+                backtrace.unshift location
+            end
+        end
+
+        filtered_backtrace = backtrace[0, backtrace.size - our_frame_pos].
+            map do |line|
+                if line =~ /load_dsl_file.*(method_missing|send)/
+                    next
+                end
+
+                if line =~ /^(.*)\(eval\):(\d+)(:.*)?/
+                    line_prefix  = $1
+                    line_number  = $2
+                    line_message = $3
+                    if line_message =~ /load_dsl_file_eval/
+                        line_message = ""
+                    end
+
+                    "#{line_prefix}#{file}:#{line_number}#{line_message}"
+                else
+                    if line =~ /load_dsl_file_eval/
+                        next
+                    else
+                        next(line) 
+                    end
+                end
+
+            end.compact
+
+        backtrace = (filtered_backtrace + backtrace[(backtrace.size - our_frame_pos)..-1])
+        raise e, message, backtrace
+    end
+
+    # Load the given file by eval-ing it in the provided binding. The
+    # originality of this method is to translate errors that are detected in the
+    # eval'ed code into 
+    #
+    # The caller of this method should call it at the end of its definition
+    # file, or the translation method may not be robust at all
     def load_dsl_file(file, binding, full_backtrace, *exceptions)
         loaded_file = file.gsub(/^#{Regexp.quote(Dir.pwd)}\//, '')
         caller_string = caller(1)[0].split(':')
