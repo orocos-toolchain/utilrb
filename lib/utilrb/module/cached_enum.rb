7489ae68fff52ff1d6626b8084580ad86ea4ce70
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2010-01-22 16:51:38 +0100

    cached enums need to be reinitialized on copy, therefore do so

--- lib/utilrb/module/cached_enum.rb
+++ lib/utilrb/module/cached_enum.rb
@@ -1,3 +1,14 @@
+module CachedValuesSupport
+    attribute(:cached_variables) { Set.new }
+    def initialize_copy(other)
+        super
+
+        cached_variables.each do |varname|
+            instance_variable_set(varname, nil)
+        end
+    end
+end
+
 class Module
     # Creates <tt>enum_#{name}</tt> method which returs an Enumerator object
     # for the <tt>each_#{enum_name}</tt> method. This enumerator is created
@@ -7,16 +18,19 @@ class Module
     # one argument, which is given in argument of the 'enum' method. In that
     # case, an enumerator is created for each argument
     def cached_enum(enum_name, name, with_arg)
+        include CachedValuesSupport
 	if with_arg
 	    class_eval <<-EOD
 		def enum_#{name}(arg)
 		    @enum_#{name} ||= Hash.new
+                    cached_variables << :@enum_#{name}
 		    @enum_#{name}[arg] ||= enum_for(:each_#{enum_name}, arg)
 		end
 		EOD
 	else
 	    class_eval <<-EOD
 		def enum_#{name}
+                    cached_variables << :@enum_#{name}
 		    @enum_#{name} ||= enum_for(:each_#{enum_name})
 		end
 		EOD
