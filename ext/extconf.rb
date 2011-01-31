f9f6f138c5e1103bf65a778a12e0f39a8b6514c3
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-01-31 08:58:22 +0100

    define RUBY_IS_RBX in the C extension when compiling for rubinius

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,6 +1,10 @@
 require 'mkmf'
 
 CONFIG['CC'] = "g++"
+if defined?(RUBY_ENGINE) && RUBY_ENGINE == "rbx"
+    $CFLAGS += " -DRUBY_IS_RBX"
+end
+
 if RUBY_VERSION >= "1.9"
     $CFLAGS += " -DRUBY_IS_19"
 end
