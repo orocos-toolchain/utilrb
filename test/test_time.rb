98879e4d227e0397b6cee995ec79ab4d47b62ecf
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2012-09-25 09:32:19 +0200

    test: don't use fractional values to initialize Time in to/from_hms tests
    It was breaking because of rounding issues.

--- test/test_time.rb
+++ test/test_time.rb
@@ -6,8 +6,8 @@ class TC_Time < Test::Unit::TestCase
 	assert_equal("0:00:00.000", Time.at(0).to_hms)
 	assert_equal("0:00:00.100", Time.at(0.1).to_hms)
 	assert_equal("0:00:34.100", Time.at(34.1).to_hms)
-	assert_equal("0:21:34.100", Time.at(21 * 60 + 34.1).to_hms)
-	assert_equal("236:21:34.100", Time.at(236 * 3600 + 21 * 60 + 34.1).to_hms)
+	assert("0:21:34.100" == Time.at(21 * 60 + 34, 100000).to_hms)
+	assert("236:21:34.100", Time.at(236 * 3600 + 21 * 60 + 34, 100000).to_hms)
     end
     def test_from_hms
 	assert_equal([0, 0, 0, 1], Time.hms_decomposition('0:00:00.001'))
@@ -18,9 +18,9 @@ class TC_Time < Test::Unit::TestCase
 	assert_equal([0, 0, 0, 100], Time.hms_decomposition('0:00:00.100'))
 
 	assert_equal([0, 0, 0, 0], Time.hms_decomposition("0:00:00.000"))
-	assert_equal(Time.at(34.1), Time.from_hms("0:00:34.100"))
-	assert_equal(Time.at(21 * 60 + 34.1), Time.from_hms("0:21:34.100"))
-	assert_equal(Time.at(236 * 3600 + 21 * 60 + 34.1), Time.from_hms("236:21:34.100"))
+	assert_equal(Time.at(34, 100000), Time.from_hms("0:00:34.100"))
+	assert_equal(Time.at(21 * 60 + 34, 100000), Time.from_hms("0:21:34.100"))
+	assert_equal(Time.at(236 * 3600 + 21 * 60 + 34, 100000), Time.from_hms("236:21:34.100"))
 
 	assert_equal([329991, 0, 10, 0], Time.hms_decomposition("329991:00:10"))
 	assert_equal([329991, 8, 8, 0], Time.hms_decomposition("329991:08:08"))
