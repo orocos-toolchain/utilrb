4971f2699252a923400b67a509d28175aab0b0fd
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2012-10-02 12:39:41 +0200

    display full documentation generation exception backtrace if rake tracing is enabled

--- lib/utilrb/rake_common.rb
+++ lib/utilrb/rake_common.rb
@@ -7,9 +7,15 @@ module Utilrb
         rescue LoadError => e
             STDERR.puts "INFO: cannot load the Hoe gem. Distribution is disabled"
             STDERR.puts "INFO: error message is: #{e.message}"
+            if ::Rake.application.options.trace
+                puts e.backtrace.join("\n  ")
+            end
         rescue Exception => e
             STDERR.puts "INFO: cannot load the Hoe gem, or Hoe fails. Distribution is disabled"
             STDERR.puts "INFO: error message is: #{e.message}"
+            if ::Rake.application.options.trace
+                puts e.backtrace.join("\n  ")
+            end
         end
 
         def self.rdoc
@@ -19,9 +25,15 @@ module Utilrb
         rescue LoadError => e
             STDERR.puts "INFO: cannot load RDoc, Documentation generation is disabled"
             STDERR.puts "INFO: error message is: #{e.message}"
+            if ::Rake.application.options.trace
+                puts e.backtrace.join("\n  ")
+            end
         rescue Exception => e
             STDERR.puts "INFO: cannot load the RDoc gem, or RDoc failed to load. Documentation generation is disabled"
             STDERR.puts "INFO: error message is: #{e.message}"
+            if ::Rake.application.options.trace
+                puts e.backtrace.join("\n  ")
+            end
         end
     end
 end
