e7c124f35e02a405e6764a10ffb2f261e3f1ec74
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2012-12-12 08:15:14 +0100

    documentation

--- lib/utilrb/logger/indent.rb
+++ lib/utilrb/logger/indent.rb
@@ -1,14 +1,44 @@
 require 'utilrb/object/attribute'
 class Logger
+    # [Integer] the current nest size
     attribute(:nest_size) { 0 }
+
+    # Sets the absolute number of spaces that should be prepended to every
+    # message
+    #
+    # You usually want to increment / decrement this with {nest}
     def nest_size=(new_value)
         @nest_string = nil
         @nest_size = new_value
     end
 
-    def nest(size, level = nil)
-        if level
-            send(level) do
+    # Adds a certain number number of spaces to the current indentation level
+    #
+    # @overload nest(size)
+    #   Permanently adds a number of spaces to the current indentation
+    #   @param [Integer] size the number of spaces that should be added to
+    #     {nest_size}
+    #
+    # @overload nest(size) { }
+    #   Adds a number of spaces to the current indentation for the duration of
+    #   the block, and restores the original indentation afterwards.
+    #
+    #   @param [Integer] size the number of spaces that should be added to
+    #     {nest_size}
+    #
+    # @overload nest(size, log_level) { }
+    #   Shortcut for
+    #   @example
+    #     
+    #     logger.send(log_level) do
+    #       logger.nest(size) do
+    #         ...
+    #       end
+    #     end
+    #
+    def nest(size, log_level = nil)
+        if log_level
+            send(log_level) do
                 nest(size) do
                     yield
                 end
@@ -29,6 +59,8 @@ class Logger
         end
     end
 
+    # Overloaded from the base logger implementation to add the current
+    # indentation
     def format_message(severity, datetime, progname, msg)
         if !@nest_string
             @nest_string = " " * self.nest_size
