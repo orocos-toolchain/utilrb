6a0bb4837269b586f87bad99ba911d4d52a3854c
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2012-12-16 13:05:03 +0100

    models: add sub-model registration code

---
+++ lib/utilrb/models/registration.rb
@@ -0,0 +1,78 @@
+require 'utilrb/logger'
+require 'utilrb/module/attr_predicate'
+module Utilrb
+    module Models
+        extend Logger::Hierarchy
+        extend Logger::Forward
+
+        # Handling of registration of model hierarchies
+        #
+        # It depends on the mixed-in object to provide a #supermodel method that
+        # returns the model that is parent of +self+
+        module Registration
+            # Tells {#clear_submodels} whether this model should be removed from
+            # the model set or not. The default is false (it should be removed)
+            #
+            # @return [Boolean]
+            attr_predicate :permanent_model?, true
+
+            # [ValueSet] the set of models that are children of this one
+            attribute(:submodels) { ValueSet.new }
+
+            # Call to register a model that is a submodel of +self+
+            def register_submodel(klass)
+                submodels << klass
+                if m = supermodel
+                    m.register_submodel(klass)
+                end
+            end
+
+            # Enumerates all models that are submodels of this class
+            def each_submodel
+                return enum_for(:each_submodel) if !block_given?
+                submodels.each do |obj|
+                    yield(obj)
+                end
+            end
+
+            # Clears all registered submodels
+            def clear_submodels
+                children = self.submodels.find_all { |m| !m.permanent_model? }
+                if !deregister_submodels(children)
+                    return
+                end
+
+                # This contains the permanent submodels
+                #
+                # We can call #clear_submodels while iterating here as it is a
+                # constraint that all models in #submodels are permanent (and
+                # will therefore not be removed)
+                submodels.each { |m| m.clear_submodels }
+                # And this the non-permanent ones
+                children.each { |m| m.clear_submodels }
+                true
+            end
+
+            # Deregisters a set of submodels on this model and all its
+            # supermodels
+            #
+            # This is usually not called directly. Use #clear_submodels instead
+            #
+            # @param [ValueSet] set the set of submodels to remove
+            def deregister_submodels(set)
+                current_size = submodels.size
+                submodels.difference!(set.to_value_set)
+                if (submodels.size != current_size)
+                    if m = supermodel
+                        m.deregister_submodels(set)
+                    end
+                    true
+                else false
+                end
+            end
+        end
+    end
+end
+
+
+
