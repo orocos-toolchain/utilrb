2507f7f10e0de67cf8361fa3b5bc2d76a7f3f362
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-09-25 08:50:44 -0300

    use a pure-ruby implementation of singleton_class?
    It only applies to ruby 2.0, and I cache the result to avoid the cost
    of #ancestors anyways.

--- lib/utilrb/module/singleton_class_p.rb
+++ lib/utilrb/module/singleton_class_p.rb
@@ -1,5 +1,14 @@
 class Module
     if !method_defined?(:singleton_class?)
-        require 'utilrb/utilrb'
+        # It so happens that this method to determine whether a class is a
+        # singleton class is valid for ruby 2.0 and breaks on 2.1 ... However
+        # (!) on 2.1 singleton_class? is defined
+        def singleton_class?
+            if instance_variable_defined?(:@__utilrb_singleton_class)
+                @__utilrb_singleton_class
+            else
+                @__utilrb_singleton_class = (ancestors.first == self)
+            end
+        end
     end
 end
